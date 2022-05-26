4d464044e29977f169374689e7af31c0557e78e6
Michael Wyraz <michael@wyraz.de>
2022-05-26 15:15:02 +0200

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='kuploy',  
-     version='0.1.0',
+     version='0.2.0',
      scripts=['kuploy'] ,
      author="Michael Wyraz",
      author_email="kuploy@michael.wyraz.de",

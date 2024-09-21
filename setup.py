705afca0c06418af3338ea5ad490fdc1fde37ebf
Michael Wyraz <michael@wyraz.de>
2024-09-21 16:28:13 +0200

    0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='kuploy',  
-     version='0.2.0',
+     version='0.3.0',
      scripts=['kuploy'] ,
      author="Michael Wyraz",
      author_email="kuploy@michael.wyraz.de",
